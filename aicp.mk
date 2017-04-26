520ef4c3443d4de0a9cc35fc27a8efc6b198254d
Seitou Saki <seitou.shrine@gmail.com>
2017-04-27 01:16:29 +0900

    AICP-ify

--- aicp.mk
+++ aicp.mk
@@ -16,9 +16,18 @@
 $(call inherit-product, device/htc/m7wlj/full_m7wlj.mk)
 
 # Inherit Lineage full phone configuration
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+-include vendor/aicp/configs/bootanimation.mk
 
 # Device naming
-PRODUCT_NAME := lineage_m7wlj
+PRODUCT_NAME := aicp_m7wlj
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTL22 BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/m7_jp_kdi/m7wlj:4.4.2/KOT49H/363468.12:user/release-keys" PRIVATE_BUILD_DESC="3.13.970.12 CL363468 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Seitou Saki(Unofficial)"
